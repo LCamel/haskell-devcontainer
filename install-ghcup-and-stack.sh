460cf880d31699b64c3e3c9dfbe723a6da319cde
Luoh Ren-Shan (LCamel) <lcamel@gmail.com>
2025-10-24 23:41:12 +0800

    add error handling and explain why not using BOOTSTRAP_HASKELL_ADJUST_BASHRC

--- install-ghcup-and-stack.sh
+++ install-ghcup-and-stack.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+set -e
 
 curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | BOOTSTRAP_HASKELL_NONINTERACTIVE=1 BOOTSTRAP_HASKELL_MINIMAL=1 sh
 
+# Manually append to both .bashrc and .zshrc instead of using BOOTSTRAP_HASKELL_ADJUST_BASHRC because:
+# 1. ghcup's adjust_bashrc uses 'sed -i' which may fail if files don't exist
+# 2. It only modifies one shell profile based on $SHELL, but users may switch shells
+# 3. Using '>>' is more robust - it creates files if they don't exist
 echo '[ -f "$HOME/.ghcup/env" ] && . "$HOME/.ghcup/env" # ghcup-env' >> ~/.bashrc
 echo '[ -f "$HOME/.ghcup/env" ] && . "$HOME/.ghcup/env" # ghcup-env' >> ~/.zshrc
 
