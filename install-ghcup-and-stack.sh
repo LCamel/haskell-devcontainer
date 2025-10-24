222703423545ada7d3ee3469eafaacf1f3062748
Luoh Ren-Shan (LCamel) <lcamel@gmail.com>
2025-10-25 00:22:41 +0800

    upgrade to lts-24.11 (ghc 9.10.2, hls 2.11.0.0) and add cabal

--- install-ghcup-and-stack.sh
+++ install-ghcup-and-stack.sh
@@ -12,18 +12,23 @@ echo '[ -f "$HOME/.ghcup/env" ] && . "$HOME/.ghcup/env" # ghcup-env' >> ~/.zshrc
 
 . $HOME/.ghcup/env
 
-# lts-23.25
-# https://raw.githubusercontent.com/commercialhaskell/stackage-snapshots/master/lts/23/25.yaml
-# snapshot version -> ghc version -> hls version
+# stackage snapshot lts-24.11
+# https://raw.githubusercontent.com/commercialhaskell/stackage-snapshots/master/lts/24/11.yaml
+# snapshot version (24.11) -> ghc version (9.10.2) -> hls version (2.11.0.0)
+# https://raw.githubusercontent.com/haskell/ghcup-metadata/master/hls-metadata-0.0.1.json
 
-time ghcup install ghc 9.8.4; ghcup set ghc 9.8.4
+time ghcup install ghc 9.10.2; ghcup set ghc 9.10.2
 ghc --version
 
-time ghcup install hls 2.10.0.0; ghcup set hls 2.10.0.0
+time ghcup install hls 2.11.0.0; ghcup set hls 2.11.0.0
 haskell-language-server-wrapper --version
 
-ghcup install stack
+# newest stack is fine
+time ghcup install stack
 stack --version
-
 stack config set system-ghc --global true
 stack config set install-ghc --global false
+
+# newest cabal is fine (stack uses its own cabal internally anyway)
+time ghcup install cabal
+cabal --version
