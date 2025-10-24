2fa9c706d138bb9877fbf29a2df92404b42aa5a8
Luoh Ren-Shan (LCamel) <lcamel@gmail.com>
2025-10-24 22:27:28 +0800

    improve install-os-packages.sh: add error handling and cleanup

--- install-os-packages.sh
+++ install-os-packages.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+set -e
+
 apt-get update
 apt-get install -y build-essential curl libffi-dev libffi8 libgmp-dev libgmp10 libncurses-dev pkg-config
+apt-get clean
+rm -rf /var/lib/apt/lists/*
