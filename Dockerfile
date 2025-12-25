4fe615fb4c4847cc53f46a5cb0970703d6d1056f
Luoh Ren-Shan (LCamel) <lcamel@gmail.com>
2025-12-25 14:28:25 +0800

    extract Haskell version configuration to haskell-versions.env

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,7 @@ RUN --mount=type=bind,source=install-os-packages.sh,target=/tmp/install-os-packa
 USER vscode
 
 RUN --mount=type=bind,source=install-haskell.sh,target=/tmp/install-haskell.sh \
+    --mount=type=bind,source=haskell-versions.env,target=/tmp/haskell-versions.env \
     bash -x /tmp/install-haskell.sh
 
 CMD ["bash"]
--- install-haskell.sh
+++ install-haskell.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 set -e
 
-# stackage snapshot lts-24.11 -> ghc 9.10.2
-# https://raw.githubusercontent.com/commercialhaskell/stackage-snapshots/master/lts/24/11.yaml
-# ghc 9.10.2 -> hls version 2.11.0.0
-# https://raw.githubusercontent.com/haskell/ghcup-metadata/master/hls-metadata-0.0.1.json
+# Load versions from external file
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+. "$SCRIPT_DIR/haskell-versions.env"
 
-GHC_VERSION=9.10.2
-HLS_VERSION=2.11.0.0
-STACK_VERSION=latest
-CABAL_VERSION=latest
 
 curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | BOOTSTRAP_HASKELL_NONINTERACTIVE=1 BOOTSTRAP_HASKELL_MINIMAL=1 sh
 
