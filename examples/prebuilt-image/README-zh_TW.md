daab02b73c48bd1384b3fd0aebd86180ebe5f92f
Luoh Ren-Shan (LCamel) <lcamel@gmail.com>
2026-01-01 18:12:44 +0800

    enhance prebuilt-image example with improved config and documentation

---
+++ examples/prebuilt-image/README-zh_TW.md
@@ -0,0 +1,9 @@
+# 使用 prebuild image
+
+請先安裝 Docker 和 VSCode.
+
+執行 `clean-start.sh`, 這會開啟一個與目前 VSCode 獨立的全新環境.
+
+如果你想沿用現有的環境, 可以直接在這個目錄執行 `code .`, 或調整 clean-start.sh 的內容.
+
+也請檢視目錄中的 JSON settings 是否符合您的需求.
--- examples/prebuilt-image/clean-start.sh
+++ examples/prebuilt-image/clean-start.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 cd "$(dirname "${BASH_SOURCE[0]}")" || exit 1
 
-# Pull the latest image
-#docker pull ghcr.io/lcamel/haskell-devcontainer:latest
-docker pull ghcr.io/lcamel/haskell-devcontainer-temp:main
+echo "This script helps you start a clean VSCode instance to test the devcontainer setup."
 
+echo "Creating temporary VSCode user data and extensions dirs..."
 TEMP_DIR=$(mktemp -d)
-mkdir -p "$TEMP_DIR/p"
-cp -R .devcontainer "$TEMP_DIR/p/.devcontainer"
-cp -R .vscode "$TEMP_DIR/p/.vscode"
-code --user-data-dir "$TEMP_DIR/u" --extensions-dir "$TEMP_DIR/e" "$TEMP_DIR/p"
+echo "Temporary directory created at: $TEMP_DIR"
+echo
+
+echo "Pre-installing the Dev Containers extension ... (to avoid the prompt)"
+code --user-data-dir "$TEMP_DIR/u" --extensions-dir "$TEMP_DIR/e"  --install-extension ms-vscode-remote.remote-containers
+echo
+
+echo "Launching VSCode with clean user data and extensions dirs ..."
+echo
+
+#echo "Choose 'Reopen in Container' from the Command Palette to open the current folder in the devcontainer."
+#echo
+#code --user-data-dir "$TEMP_DIR/u" --extensions-dir "$TEMP_DIR/e" .
+
+echo "Entering the devcontainer directly ..."
+echo
+code --user-data-dir "$TEMP_DIR/u" --extensions-dir "$TEMP_DIR/e" --folder-uri="vscode-remote://dev-container+$(pwd | tr -d '\n' | xxd -c 256 -p)/workspaces/haskell-devcontainer/examples/prebuilt-image" # path from the git root
 
