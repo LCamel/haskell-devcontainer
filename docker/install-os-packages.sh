630dfa289a561c2ac60a14537bf43ae4398bbcdd
Luoh Ren-Shan (LCamel) <lcamel@gmail.com>
2026-01-01 22:59:17 +0800

    add DEBIAN_FRONTEND=noninteractive

--- docker/install-os-packages.sh
+++ docker/install-os-packages.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+export DEBIAN_FRONTEND=noninteractive
+
 apt-get update
 apt-get install -y build-essential curl libffi-dev libffi8 libgmp-dev libgmp10 libncurses-dev pkg-config
 apt-get clean
