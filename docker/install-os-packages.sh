582eeedc43b0c2d82417f7e43c9324680ffcdd47
Luoh Ren-Shan (LCamel) <lcamel@gmail.com>
2026-01-02 23:59:15 +0800

    add comment about "ghcup tool-requirements --raw-format"

--- docker/install-os-packages.sh
+++ docker/install-os-packages.sh
@@ -3,6 +3,7 @@ set -e
 
 export DEBIAN_FRONTEND=noninteractive
 
+# This list can be obtained from `ghcup tool-requirements --raw-format`
 apt-get update
 apt-get install -y build-essential curl libffi-dev libffi8 libgmp-dev libgmp10 libncurses-dev pkg-config
 apt-get clean
